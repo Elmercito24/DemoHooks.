ce5f89d019013f8acf9deae4527b67e0efa0a8d9
Elmer Vásquez Guevara <vasquezguevaraelmer75@gmail.com>
2024-11-28 11:05:01 -0500

    Demostracion

--- DemoHooks/DemoHooks/Program.cs
+++ DemoHooks/DemoHooks/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Net.Security;
+
+           Console.WriteLine("Hola mundo");
+
+     Console.WriteLine("Prueba de formateo de codigo");
+
+          int Edad = 22;
+
+
+
+                       if (Edad >= 18)
+ {
+    Console.WriteLine("Es mayor de edad tiene: {0} años de edad ", Edad);
+                             }
+           else
+      {
+    Console.WriteLine("Es menor de edad tiene: {0}", Edad);
+     }
